71971278c023c31cdc492c0840a737d1972700f2
Etienne RIGAUD <40430009+erigaud@users.noreply.github.com>
2019-01-09 09:30:28 +0100

    Update remplirtables.php

--- ProjetBdD/remplirtables.php
+++ ProjetBdD/remplirtables.php
@@ -1,4 +1,13 @@
+<?php
 
+// Connexion à la base de données
+ 
+	define( 'DB_NAME', 'Livres' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', '' );
+	define( 'DB_HOST', 'localhost' );
+	
+?>
 
 <!DOCTYPE html>
 <html>
@@ -8,8 +17,102 @@
 	</head>
 	
 	<body>
+	
 	<form method="post">
-	<input type="file" text="Créer" name="Créer base">
+	Auteur <input type="file" name="table_auteur" required/><br/>
+	Editeur <input type="file" name="table_editeur"required/><br/>
+	Livre <input type="file" name="table_livre"required/><br/>
+	Ecrit par <input type="file" name="table_ecriture" required/><br/>
+	Edité par <input type="file" name="table_edition" required/><br/>
+	<input type="submit" name="commencer_remplir" value="Remplir table"/>
 	</form>
+	
+	<?php 
+	if(isset($_POST['commencer_remplir'])){
+		if(isset($_POST['table_auteur'])){
+			
+				insererAuteur($_POST['table_auteur']);
+				
+			}
+		
+		if(isset($_POST['table_editeur'])){
+			
+			insererEditeur($_POST['table_editeur']);
+				
+			}
+		
+		if(isset($_POST['table_livre'])){
+			
+			insererLivre($_POST['table_livre']);
+				
+			}
+		
+		if(isset($_POST['table_ecriture'])){
+			//$handler = fopen($_POST['table_ecriture'],"r");
+			
+			insererEditePar($_POST['table_ecriture']);
+			
+			}
+		
+		if(isset($_POST['table_edition'])){
+			//$handler = fopen($_POST['table_edition'],"r");
+			
+			insererEcritPar($_POST['table_edition']);
+			
+			}
+		
+		header('Location: index.php');
+	}
+	
+	function insererEditeur($file){
+			$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD,array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+			//$req = $connexion->prepare('INSERT ignore INTO editeur(nom_éditeur,site_web) VALUES(:nom_editeur, :siteweb)');
+			$req = $connexion->prepare("LOAD DATA LOCAL INFILE :filename into table editeur FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES");
+
+			/*$req->execute(array(
+				'filename'=>$file,
+				'nom_editeur' => $nom,
+				'siteweb' => $site));*/
+				
+			$req->execute(array(
+				'filename'=>$file)); 
+	}
+	
+	function insererAuteur($file){
+		$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD,array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+		$req = $connexion->prepare("LOAD DATA LOCAL INFILE :filename into table auteur FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES");
+		$req->execute(array(
+				'filename'=>$file)); 	
+	}
+	
+	function insererLivre($file){
+		$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD,array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+				$req = $connexion->prepare("LOAD DATA LOCAL INFILE :filename into table livre FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES");
+
+				$req->execute(array(
+				'filename'=>$file)); 
+	}
+	function insererEditePar($file){
+		$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD,array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+		$req = $connexion->prepare("LOAD DATA LOCAL INFILE :filename into table edition FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES");
+
+		$req->execute(array(
+				'filename'=>$file)); 
+	}
+	function insererEcritPar($file){
+		$connexion = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASSWORD,array(
+        PDO::MYSQL_ATTR_LOCAL_INFILE => true));
+		$req = $connexion->prepare("LOAD DATA LOCAL INFILE :filename into table ecriture FIELDS TERMINATED BY ';' ENCLOSED BY '\"' IGNORE 1 LINES");
+
+		$req->execute(array(
+				'filename'=>$file)); 
+	}
+	
+	?>
+	
 	</body>
 </html>
